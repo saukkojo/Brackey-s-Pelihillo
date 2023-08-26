eb6634afc3cf4b19e716f661b007da327639e188
konsimo <simo.kontiokorpi@gmail.com>
2023-08-26 18:09:17 +0300

    Added Pearl Pick-Up VFX

--- Brackey's pelihillo/Assets/Code/Entities/Collectibles/Currency.cs
+++ Brackey's pelihillo/Assets/Code/Entities/Collectibles/Currency.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Currency : Collectible, ICurrency
 {
     [SerializeField] private int _value = 10;
     public int value => _value;
+    [SerializeField] private GameObject pickUpEffect;
 
     public override void Collect()
     {
         base.Collect();
+        GameManager.current.bank.AddFunds(value);
+
+        GameObject effect = Instantiate(pickUpEffect, this.transform.position, Quaternion.identity);
+        //Destroy(pickUpEffect, 3f);
     }
 }
