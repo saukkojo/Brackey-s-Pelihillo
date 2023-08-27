65f3d28858442daa25fd34be6bd2b660645550a9
bafoonn <tuomas.antikainen@outlook.com>
2023-08-27 10:40:41 +0300

    added lights

--- Brackey's pelihillo/Assets/Code/WaterDepthChanger.cs
+++ Brackey's pelihillo/Assets/Code/WaterDepthChanger.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class WaterDepthChanger : MonoBehaviour
 {
     public Material _mat;
     private Player _player;
     private ModuleHandler _handler;
+    public Light2D _light;
 
     private void Awake()
     {
+        if (_light == null)
+        {
+            _light = GetComponent<Light2D>();
+        }
         Debug.Assert(_mat != null);
+        Debug.Assert(_light != null);
     }
 
     private void Start()
@@ -28,5 +35,6 @@ public class WaterDepthChanger : MonoBehaviour
     {
         float t = _player.depth / _handler.maxDepth;
         _mat.SetFloat("_Depth", t);
+        _light.intensity = 1 - t;
     }
 }
